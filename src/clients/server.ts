6a5c14680cc469718a60cc37f05d77868170b227
csantander93 <casdesarrollo.2022@gmail.com>
2024-12-30 03:14:45 -0300

    deploy listo

--- src/clients/server.ts
+++ src/clients/server.ts
@@ -1,15 +1,16 @@
-// server.ts
 import axios from 'axios';
 
+const baseURL ='https://task-manager-back-production-27ea.up.railway.app/api';
+
 export const httpServer = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL,
   headers: {
     'Content-type': 'application/json',
   },
 });
 
 export const httpServerMultipart = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL,
   headers: {
     'Content-type': 'multipart/form-data',
   },
